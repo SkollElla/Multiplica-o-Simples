73f4087155c9e608b1bdca17cff0013f26052988
Luan Carlos <99558903+SkollElla@users.noreply.github.com>
2022-11-09 19:54:25 -0300

    Add files via upload

---
+++ Multiplicacao_Simples/Multiplicação Simples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a, b, prodm;
+
+        Console.WriteLine("Digite o primeiro número!");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo número!");
+        b = int.Parse(Console.ReadLine());
+        prodm = Convert.ToInt32(a * b);
+        Console.WriteLine("O Produto da multiplicacao é igual a : " + prodm);
+     
+  ;
+    }
+}
